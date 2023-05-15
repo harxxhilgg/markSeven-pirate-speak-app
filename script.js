99bfbfd63c5ccfd5138a8e35e165d33b268bef15
Harshil Patel <120940967+harxxhilgg@users.noreply.github.com>
2023-05-16 01:21:24 +0530

    Pirate Translate webapp done!
    Note: not responsive

---
+++ script.js
@@ -0,0 +1,24 @@
+var input = document.getElementById('inputBox');
+var button = document.getElementById('translateBtn');
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    alert("something broke!");
+}
+
+button.addEventListener("click", () => {
+    var inputText = input.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        document.getElementById('outputBox').innerHTML = translatedText;
+    })
+    .catch(errorHandler)
+})
